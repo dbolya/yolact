95ac8af6685526e2c99198b4cb916d372440be2d
Daniel Bolya <dbolya@gmail.com>
2018-07-09 00:31:23 -0700

    Fixed a coco class in the annotations ('fire', 'hydrant' -> 'fire hydrant')

--- data/coco.py
+++ data/coco.py
@@ -14,7 +14,7 @@ ANNOTATIONS = 'annotations'
 COCO_API = 'PythonAPI'
 INSTANCES_SET = 'instances_{}.json'
 COCO_CLASSES = ('person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
-                'train', 'truck', 'boat', 'traffic light', 'fire', 'hydrant',
+                'train', 'truck', 'boat', 'traffic light', 'fire hydrant',
                 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog',
                 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra',
                 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
@@ -27,7 +27,7 @@ COCO_CLASSES = ('person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
                 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
                 'keyboard', 'cell phone', 'microwave oven', 'toaster', 'sink',
                 'refrigerator', 'book', 'clock', 'vase', 'scissors',
-                'teddy bear', 'hair drier', 'toothbrush')
+                'teddy bear', 'hair drier', 'toothbrush', 'hair brush')
 
 
 def get_label_map(label_file):
