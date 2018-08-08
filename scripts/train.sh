f6ad7a11388fa4737fb9b8f65928fe8dba05c404
Daniel Bolya <dbolya@gmail.com>
2018-08-07 21:30:41 -0700

    Added automatic flushing to training script.

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,14 +2,13 @@
 #SBATCH -p GPU-shared
 #SBATCH -t 48:00:00
 #SBATCH --gres=gpu:p100:1
+#SBATCH --no-requeue
 
 # Usage: ./train.sh config batch_size
-
-set -x
 
 module load python/3.6.4_gcc5_np1.14.5
 module load cuda/9.2
 
 cd $SCRATCH/yolact
 
-python3 train.py --config $1 --batch_size $2 --save_interval 5000 &>logs/$1_log
+stdbuf -oL python3 train.py --config $1 --batch_size $2 --save_interval 5000 &>logs/$1_log
