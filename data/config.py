0f028294cc68bcd2c2b7dfe74c778cf2e2f24dd4
Daniel Bolya <dbolya@gmail.com>
2019-02-08 03:55:29 -0800

    I was too ambitious.

--- data/config.py
+++ data/config.py
@@ -1020,8 +1020,8 @@ yrm35_bigimg_config = yrm36_retina_config.copy({
     'use_focal_loss': False,
     'conf_alpha': coco_base_config.conf_alpha,
 
-    'max_size': 1024,
-    'min_size': 1024,
+    'max_size': 800,
+    'min_size': 800,
 
     'freeze_bn': True,
 })
