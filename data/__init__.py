47232a06856df2b23cd84970544073d0b63f1210
Daniel Bolya <dbolya@gmail.com>
2019-04-24 23:23:36 -0700

    Removed the VOC stuff because we don't support VOC.

--- data/__init__.py
+++ data/__init__.py
@@ -1,5 +1,3 @@
-from .voc0712 import VOCDetection, VOCAnnotationTransform, VOC_CLASSES, VOC_ROOT
-
 from .coco import COCODetection, COCOAnnotationTransform, COCO_CLASSES, get_label_map
 from .config import *
 
