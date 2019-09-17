a538dc65fc9c1dc1b201505d5e4a35a1ef780338
Agade09 <n.sillitoe@gmail.com>
2019-09-17 18:14:05 +0200

    Added an option to augment data with [90,180,270] degree rotations

--- data/config.py
+++ data/config.py
@@ -451,6 +451,8 @@ coco_base_config = Config({
     'augment_random_mirror': True,
     # Flip the image vertically with a probability of 1/2
     'augment_random_flip': False,
+    # With a probability of 1/2, rotate the image [90,180,270] degrees (uniform choice between the three)
+    'augment_random_rot90': False,
 
     # If using batchnorm anywhere in the backbone, freeze the batchnorm layer during training.
     # Note: any additional batch norm layers after the backbone will not be frozen.
