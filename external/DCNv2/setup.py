eee5ce36242fa1de808a287a3cfe6055486e42b5
Sharif Elfouly <selfouly@gmail.com>
2020-02-05 00:05:06 +0100

    Update setup.py (#313)

--- external/DCNv2/setup.py
+++ external/DCNv2/setup.py
@@ -38,7 +38,7 @@ def get_extensions():
             "-D__CUDA_NO_HALF2_OPERATORS__",
         ]
     else:
-        raise NotImplementedError('Cuda is not availabel')
+        raise NotImplementedError('Cuda is not available')
 
     sources = [os.path.join(extensions_dir, s) for s in sources]
     include_dirs = [extensions_dir]
