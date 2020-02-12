fccd89b273d641429a2abc77ee07e28a0db35f53
Marek Otahal <markotahal@gmail.com>
2020-02-12 09:24:30 +0100

    ScriptModuleWrapper in a sepatate file
    and imported where needed

--- layers/modules/fpn.py
+++ layers/modules/fpn.py
@@ -7,14 +7,7 @@ from typing import List
 
 #local imports
 from data.config import Config
-
-#  As of March 10, 2019, Pytorch DataParallel still doesn't support JIT Script Modules
-use_jit = torch.cuda.device_count() <= 1
-if not use_jit:
-    print('Multiple GPUs detected! Turning off JIT.')
-
-ScriptModuleWrapper = torch.jit.ScriptModule if use_jit else nn.Module
-script_method_wrapper = torch.jit.script_method if use_jit else lambda fn, _rcn=None: fn
+from utils.script_module_wrapper import ScriptModuleWrapper, script_method_wrapper
 
 
 
