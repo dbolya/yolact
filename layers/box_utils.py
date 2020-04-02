b97e82d809e5e69dc628930070a44442fd23617a
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-04-01 19:16:18 -0700

    Update box_utils.py (#367)
    * Update box_utils.py
    
    Using .prod() rather than indexing may be faster. I've seen speedups in normal pytorch code after this change, not sure if the jit script affects this.

--- layers/box_utils.py
+++ layers/box_utils.py
@@ -48,8 +48,7 @@ def intersect(box_a, box_b):
                        box_b[:, :, 2:].unsqueeze(1).expand(n, A, B, 2))
     min_xy = torch.max(box_a[:, :, :2].unsqueeze(2).expand(n, A, B, 2),
                        box_b[:, :, :2].unsqueeze(1).expand(n, A, B, 2))
-    inter = torch.clamp((max_xy - min_xy), min=0)
-    return inter[:, :, :, 0] * inter[:, :, :, 1]
+    return torch.clamp(max_xy - min_xy, min=0).prod(3)  # inter
 
 
 def jaccard(box_a, box_b, iscrowd:bool=False):
