d9399a284b1c4c876a9d533ae16d67a1298d8e05
Baptiste Amato <baptiste.amato@gmail.com>
2020-09-19 16:32:17 +0200

    Make repo a library.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='yolact',
+      version='1.0.0',
+      author="Psycle Research",
+      description="Fork of yolact",
+      url="https://github.com/PsycleResearch/yolact",
+      packages=['yolact'],
+      python_requires='>=3.6'
+      )
