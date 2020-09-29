158af776bdbfb43399884fe301ebfb146c802fd7
Baptiste Amato <baptiste.amato@gmail.com>
2020-09-29 09:21:19 +0200

    Remove cuda checking.

--- yolact/yolact.py
+++ yolact/yolact.py
@@ -17,10 +17,6 @@ import torch.backends.cudnn as cudnn
 from yolact.utils import timer
 from yolact.utils.functions import MovingAverage, make_net
 
-# This is required for Pytorch 1.0.1 on Windows to initialize Cuda on some driver versions.
-# See the bug report here: https://github.com/pytorch/pytorch/issues/17108
-torch.cuda.current_device()
-
 # As of March 10, 2019, Pytorch DataParallel still doesn't support JIT Script Modules
 use_jit = torch.cuda.device_count() <= 1
 if not use_jit:
