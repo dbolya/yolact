c24ea45c20471fd0805c23c4bb30c6912045b26a
Natan Danous <natan.danous@gmail.com>
2021-10-29 09:45:18 +0200

    Add tooling for dist & release

--- setup.py
+++ setup.py
@@ -1,10 +1,29 @@
-from setuptools import setup
+import os
+from setuptools import setup, find_packages
 
-setup(name='yolact',
-      version='2.1.0',
-      author="Psycle Research",
-      description="Fork of yolact",
-      url="https://github.com/PsycleResearch/yolact",
-      packages=['yolact', 'yolact.utils', 'yolact.data', 'yolact.layers', 'yolact.layers.functions', 'yolact.layers.modules'],
-      python_requires='>=3.6'
-      )
+from yolact import __version__
+
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname), "rb") as fid:
+        return fid.read().decode("utf-8")
+
+
+req = read("requirements.txt").splitlines()
+dev_req = read("requirements-dev.txt").splitlines()[2:]
+requirements = req + ["setuptools"]
+
+
+setup(
+    name='yolact',
+    version=__version__,
+    author="Psycle Research",
+    description="Fork of yolact",
+    url="https://github.com/PsycleResearch/yolact",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=requirements,
+    extras_require={
+        'dev': dev_req
+    },
+    python_requires=">=3.6"
+)
