eb2208ca4fdca406382e91849ce9cc5dc6ab892b
Baptiste Amato <baptiste.amato@gmail.com>
2021-10-29 14:49:07 +0200

    Add test streamlit.

---
+++ streamlit/main.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import cv2
+import numpy as np
+
+
+def infer_segmentation(weights_path: str, im: np.ndarray, confidence: float):
+    from Repsycle.inference.detection.yolact_utils import PsycleYolact
+    model = PsycleYolact(
+        weights=weights_path,
+        preprocessing_file_path=None,
+        conf_thresh=confidence,
+        classes={'0': {'name': '0', 'color': [255, 0, 0]},
+                 '1': {'name': '1', 'color': [0, 255, 0]},
+                 '2': {'name': '2', 'color': [0, 0, 255]}},
+    )
+    out = model.infer(im)
+    im_out = model.draw(im, out)
+    cv2.imwrite('/tmp/in.png', im)
+    cv2.imwrite('/tmp/out.png', im_out)
+    return im_out
+
+
+if __name__ == '__main__':
+    st.title('Test weights file on image')
+    weights_path = st.text_input("Weights file path")
+    upload_image = st.file_uploader("Image")
+    confidence = st.slider('Confidence threshold', 0, 100, 1)
+    if st.button('Go') and weights_path and upload_image and confidence:
+        # Image
+        im = cv2.imdecode(np.asarray(bytearray(upload_image.read()), dtype=np.uint8), 1)
+        im_out = infer_segmentation(weights_path, im, confidence / 100.)
+        st.image(im_out, channels='BGR')
+
