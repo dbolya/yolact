901c142c45646995606a53bcf4fe78db599d349a
Baptiste Amato <baptiste.amato@gmail.com>
2021-11-05 11:39:19 +0100

    Fix.

--- yolact/yolact.py
+++ yolact/yolact.py
@@ -481,7 +481,10 @@ class Yolact(nn.Module):
 
     def load_weights(self, path):
         """ Loads weights from a compressed save file. """
-        state_dict = torch.load(path, map_location='cpu' if not torch.cuda.is_available() else '0')
+        if not torch.cuda.is_available():
+            state_dict = torch.load(path, map_location='cpu')
+        else:
+            state_dict = torch.load(path)
 
         # For backward compatability, remove these (the new variable is called layers)
         for key in list(state_dict.keys()):
