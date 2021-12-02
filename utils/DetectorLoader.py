c4a4a05fa43f2d0d55b558bf7de983158b14c7f7
Ajay Bhargava <ajay@actuate.ai>
2021-12-02 22:01:49 +0000

    building DetectorLoader for minimial model

---
+++ utils/DetectorLoader.py
@@ -0,0 +1,70 @@
+from yolact import Yolact
+import torch
+from data import cfg, set_cfg
+import torch.backends.cudnn as cudnn
+
+__author__ = 'Ajay Bhargava'
+__version__ = '0.1'
+
+class YOLACT(object):
+    '''
+    Class for handling YOLACT model.
+    def __init__():
+        
+        Arguments
+        --------------------------------
+        weights_file (str): location of the weights file
+        config (str): name of the base-network upon which the model is built
+        device (str): device to use the model, default is cuda, can be specified as CPU
+
+
+
+    '''
+    def __init__(self, 
+                weights_file = '../weights/yolact_resnet50_54_800000.pth',
+                config = 'yolact_resnet50_config',
+                device = 'cuda', 
+                fast_nms = True,
+                detect = False,
+                cross_class_nms = False, 
+                mask_proto_debug = False):
+
+        # Declare Configs
+        set_cfg(config)
+
+        # Set Detection
+        if detect:
+            cfg.eval_mask_branch = False
+        
+        # Declare CUDA 
+        if device == 'cuda':
+            cudnn.fastest = True
+            torch.set_default_tensor_type('torch.cuda.FloatTensor')
+        else:
+            torch.set_default_tensor_type('torch.FloatTensor')
+
+        # Initialize Model
+        self.model = Yolact()
+
+        # Load model on CUDA 
+        if device == 'cuda':
+            self.model = self.model.cuda()
+
+        self.model.load_weights(weights_file)
+        self.model.eval()
+        
+        # Other configurations specific for YOLACT 
+        if fast_nms:
+            self.model.detect.use_fast_nms = fast_nms
+
+        if cross_class_nms:
+            self.model.detect.use_cross_class_nms = cross_class_nms
+
+        if mask_proto_debug:
+            self.model.detect.mask_proto_debug = mask_proto_debug
+    
+    def detect(args):
+        '''
+
+        '''
+        
---
+++ utils/VideoLoader.py
@@ -0,0 +1,40 @@
+# Define a class for working on videos
+class VideoReader(object):
+    '''
+    Class docstring for VideoReader():
+    Provides a generator for video frames. Returns a numpy array in BGR format. 
+    '''
+    def __init__(self, file_name):
+        self.file_name = file_name
+        try:  # OpenCV parses an integer to read a webcam. Supplying '0' will use webcam. 
+            self.file_name = int(file_name)
+        except ValueError:
+            pass
+
+        self.read = cv2.VideoCapture(self.file_name)
+        self.shapes = []
+        while(self.read.isOpened()):
+            ret, frame = self.read.read()
+            if ret == True:
+                self.shapes.append(frame.shape) # numpy style shape (H x W x C). 
+            else:
+                break
+        self.read.release()
+
+    def __iter__(self):
+        self.cap = cv2.VideoCapture(self.file_name)
+        if not self.cap.isOpened():
+            raise IOError('Video {} cannot be opened'.format(self.file_name))
+        return self
+
+    def __next__(self):
+        was_read, img = self.cap.read()
+        if not was_read:
+            raise StopIteration
+        return img
+
+    def shape(self):        
+        if not all(self.x == self.shapes[0] for self.x in self.shapes):
+            raise IOError('Uneven frame sizes in {}'.format(self.file_name))
+        
+        return tuple(list(map(max, zip(*self.shapes))))
