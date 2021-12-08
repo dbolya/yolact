068dbc2fc6c5a8d0baaa46318192c3f8796014cd
Ajay Bhargava <ajay@actuate.ai>
2021-12-08 23:22:57 +0000

    figured out SORT algorithm

--- SORT/sort.py
+++ SORT/sort.py
@@ -40,7 +40,7 @@ def linear_assignment(cost_matrix):
     return np.array([[y[i],i] for i in x if i >= 0]) #
   except ImportError:
     from scipy.optimize import linear_sum_assignment
-    x, y = linear_sum_assignment(cost_matrix)
+    x, y = linear_sum_assignment(cost_matrix) # Linear Sum assignment parameters  ... LOOK HERE. 
     return np.array(list(zip(x, y)))
 
 
